149f6ba31accb47e804f4e9998e7f10a1f0d60ff
drakoczy <58729827+drakoczy@users.noreply.github.com>
2022-05-12 15:36:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*html input érték megjelenése a logban
+gomb responsive: pl. az input érték kétszeresét kiírja a logba
+a reszponzív funkcióba beletenni a feltételeket
+	9 karakter
+	csak szám
+	a 9. karakterre vonatkozó értékszabály*/
+
